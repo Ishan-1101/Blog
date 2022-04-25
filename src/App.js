7924a4cbd8fbd1a83fbd02de7559aa3704f52be8
Ishan Sharma <1905614@kiit.ac.in>
2022-04-25 16:19:40 +0530

    version 1.0.2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Card from "./components/Card";
 import Home from "./components/Home";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import Blogs from "./components/Blogs";
 
 const App = () => {
   return (
@@ -28,28 +28,10 @@ const App = () => {
         </Navbar>
 
         <Routes>
-
-        <Route
-            path="/"
-            element={
-              <Home />
-            }
-          />
-
-          <Route
-            path="/blogs"
-            element={
-              <Card
-                title1="Redux for Beginners"
-                imageUrl1="https://i0.wp.com/regroove.ca/wp-content/uploads/2018/07/react-redux-2.png?fit=2560%2C1440&ssl=1"
-                body1="Redux is an open-source JavaScript library for managing and centralizing application state."
-                title2="React-Router"
-                imageUrl2="https://images.unsplash.com/photo-1581276879432-15e50529f34b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-                body2="React Router is a standard library for routing in React. It enables the navigation among views of various components in a React Application."
-              />
-            }
-          />
+          <Route path="/" element={<Home />} />
+          <Route path="/blogs" element={<Blogs />} />
         </Routes>
+        
       </Router>
     </div>
   );
---
+++ src/components/Blogs.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Card from "./Card";
+import "./Blogs.css"
+
+const Blogs = () => {
+  return (
+    <div className="blog-container">
+      <Card
+        imageUrl="https://i0.wp.com/regroove.ca/wp-content/uploads/2018/07/react-redux-2.png?fit=2560%2C1440&ssl=1"
+        title="Redux for Beginners"
+        body="Redux is an open-source JavaScript library for managing and centralizing application state."
+        blogurl="https://ishans.notion.site/Redux-ceef36ab99d345e3be396504aac5f22d"
+      />
+      <Card
+        imageUrl="https://images.unsplash.com/photo-1581276879432-15e50529f34b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+        title="React-Router"
+        body="React Router is a standard library for routing in React. It enables the navigation among views of various components in a React Application."
+        blogurl="https://ishans.notion.site/React-Router-f8c2f5fda4604978889465b60ec9ae4e"
+      />
+    </div>
+  );
+};
+
+export default Blogs;
